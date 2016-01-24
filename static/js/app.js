4a231f2724d23efca9abb0f94173b6aeb4f8f48c
Sil Westerveld <swesterveld@minddistrict.com>
2016-01-24 19:48:34 +0100

    Set web-client id

--- static/js/app.js
+++ static/js/app.js
@@ -86,7 +86,7 @@ app.constant('HTTP_ERRORS', {
  */
 app.factory('oauth2Provider', function ($modal) {
     var oauth2Provider = {
-        CLIENT_ID: 'web-client-id',
+        CLIENT_ID: '795810892321-t4kv2l93crch7kdorp0b98vgvu3tokn0.apps.googleusercontent.com',
         SCOPES: 'email profile',
         signedIn: false
     }
